ec4b23eb9698953dcf7b46103c09873065f2852c
Yana <myalisia@gmail.com>
2024-03-22 22:23:03 +0200

    change base url

--- frontend/src/features/axios/client.ts
+++ frontend/src/features/axios/client.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-// const BASE_URL = '/';
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = '/';
+// const BASE_URL = 'http://localhost:8080';
 export const axiosInstance = axios.create({ baseURL: BASE_URL });
